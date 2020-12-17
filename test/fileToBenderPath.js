b47c0c5ad4066c1200651c603d52651d72ed2546
sculpt0r <b.kalemba@cksource.com>
2020-12-17 15:27:14 +0100

    Add test for manual tests changes

--- test/fileToBenderPath.js
+++ test/fileToBenderPath.js
@@ -2,10 +2,25 @@ var assert = require('assert');
 const {getBenderAffectedPaths} = require('../src/diff/analyzeDiff');
 
 describe('bender paths extractor', function() {
-	it('returns test path for file if non plugin test file was added',
+	it('returns test path for file if ticket test file was added',
 		MakePathTest(
 			[ [ 'A', 'tests/tickets/174/1.js' ] ],
 			'tests/tickets/174/1'
+		)
+	);
+
+	it('returns test path for plugin if plugin tests file was modified',
+		MakePathTest(
+			[ [ 'M', 'tests/plugins/ajax/ajax.js' ] ],
+			'tests/plugins/ajax/ajax'
+		)
+	);
+
+	it('do not includes test if manual test was modified',
+		MakePathTest(
+			[['M', 'tests/plugins/ajax/manual/optionalcallback.html']],
+			'tests/plugins/ajax/manual/optionalcallback',
+			false
 		)
 	);
 
@@ -16,15 +31,17 @@ describe('bender paths extractor', function() {
 			false
 		)
 	);
+
 });
 
 
 
 function MakePathTest(changedFiles, expectedPath, exists) {
-	const shouldInclude = exists === undefined ? true : exists;	
-	return function () {
-		const benderPath = getBenderAffectedPaths(changedFiles);
+	const shouldInclude = exists === undefined ? true : exists;
 
-		assert.strictEqual(benderPath.includes(expectedPath), shouldInclude);
-	}
+	return function () {
+		const benderPaths = getBenderAffectedPaths(changedFiles);
+		console.log('***' , benderPaths);
+		assert.strictEqual(benderPaths.includes(expectedPath), shouldInclude );
+	};
 }
