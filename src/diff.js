060d78e3634ac10ff3d14fd0317444238eca7dc6
sculpt0r <b.kalemba@cksource.com>
2020-12-17 14:45:11 +0100

    Add some parser changes

--- src/diff.js
+++ src/diff.js
@@ -1,9 +1,24 @@
 const { spawn } = require("child_process");
+const { parseGitOutput, getBenderAffectedPaths } = require('./diff/analyzeDiff');
 
 
+const targetBranch = process.argv[2] || 'master';
+const currentBranch = process.argv[3] || '';
 
-const ls = spawn('git diff', ['--name-only']);
+console.log(targetBranch, currentBranch);
+const ls = spawn( 'git',['diff', `${targetBranch}..${currentBranch}`,'--name-status']);
 
 ls.stdout.on("data", data => {
-    console.log(`stdout: ${data}`);
+	// console.log(data.toString());
+	const filesStatus = parseGitOutput(data.toString());
+	getBenderAffectedPaths(filesStatus);
+// console.log(`stdout: ${data}`);
+});
+
+ls.on('error', (error) => {
+	console.log(`error: ${error.message}`, error);
+});
+
+ls.on("close", (code, b) => {
+	console.log(`child process exited with code ${code}`, b);
 });
--- src/diff/analyzeDiff.js
+++ src/diff/analyzeDiff.js
@@ -1,7 +1,33 @@
-function parseGitOutput( gitOutput) {
-    console.log(gitOutput);
+function parseGitOutput( gitOutput ) {
+	const filesStatus = gitOutput.split('\n').filter( s => s !== '');
+
+	const maped = filesStatus.map(fileStatus => {
+	   return fileStatus.split('\t');
+	});
+
+	return maped;
+}
+
+function getBenderAffectedPaths(filesStatus) {
+	console.log(filesStatus);
+	const benderPaths = [];
+	filesStatus.forEach(element => {
+		const filePath = element[1].slice(0, -3);
+		//test is affected
+		if(filePath.startsWith('tests/') &&
+			!filePath.includes('/manuals/')
+			)
+		{
+			benderPaths.push(filePath);
+		}
+		//plugin is affected ->
+		//core is affected
+
+		//other non relevant
+	});
 }
 
 module.exports = {
-    parseGitOutput
+	parseGitOutput,
+	getBenderAffectedPaths
 };
